34a65573c811cfe30280a9d7c7babade7779f238
paskasiregar <paska.siregar@gmail.com>
2024-09-06 10:40:01 +0700

    6sep-added privacy policy

--- .history/src/app/privacy/page_20240906103914.js
+++ .history/src/app/privacy/page_20240906103914.js
@@ -1,6 +1,6 @@
 import React from "react";
 import CTA from "@/components/CTA";
-import { Footer } from "@/components/footer";
+import Footer from "@/components/footer";
 import Link from "next/link";
 
 export default async function Privacy() {
