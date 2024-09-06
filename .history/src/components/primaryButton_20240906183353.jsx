8b1ee6722e50987c43ddd63793f3035cd41014eb
paskasiregar <paska.siregar@gmail.com>
2024-09-06 18:34:13 +0700

    6sep-added team-v6

--- .history/src/components/primaryButton_20240906183353.jsx
+++ .history/src/components/primaryButton_20240906183353.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const PrimaryButton = ({ children, onClick, href }) => {
   return (
     <button
-      className="bg-inherit-320071 text-white font-bold py-2 px-4 rounded-lg w-full"
+      className="bg-inherit-320071 text-white font-bold py-2 px-4 rounded-lg"
       onClick={onClick}
     >
       {children}
