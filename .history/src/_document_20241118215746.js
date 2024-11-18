3349273060403c5850c4348ae1f93d31527d2245
paskasiregar <paska.siregar@gmail.com>
2024-11-18 21:57:50 +0700

    18nov-v1-added Clickadu

---
+++ .history/src/_document_20241118215746.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="clckd" content="75dc524e2732cc4c467a23fcc67502c5" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
