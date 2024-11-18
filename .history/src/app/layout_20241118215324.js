6c4c07939d2bb5f3ce415d79cb13a6e56943eff1
paskasiregar <paska.siregar@gmail.com>
2024-11-18 21:55:40 +0700

    18nov-v1-added Clickadu

---
+++ .history/src/app/layout_20241118215324.js
@@ -0,0 +1,31 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
+import Head from "next/head";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "PawPaw Portal | Your One-Stop Shelter",
+  description: "Your One-Stop Shelter",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <Head>
+        <meta name="clckd" content="75dc524e2732cc4c467a23fcc67502c5" />
+        {/* <meta
+          name="google-adsense-account"
+          content="ca-pub-4770655134097556"
+        ></meta> */}
+      </Head>
+      <body className={inter.className}>{children}</body>
+      {/* <Script
+        type="text/javascript"
+        src="//pl24310112.cpmrevenuegate.com/cc/a3/1e/cca31e5e9811b252527560f4431eff54.js"
+      ></Script> */}
+    </html>
+  );
+}
